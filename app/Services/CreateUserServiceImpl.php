f568b2128efcfd2968b5f5ea6446b22a2782b623
samuel_trindade <samuel_trindadexd@hotmail.com>
2022-07-13 16:44:45 -0300

    feat[service/getUser]: checking if theres already an user in the database. Garupa test

--- app/Services/CreateUserServiceImpl.php
+++ app/Services/CreateUserServiceImpl.php
@@ -20,6 +20,17 @@ class CreateUserServiceImpl implements CreateUserService
 
     public function createUser(\stdClass $data): ReturnCreateUserDTO
     {
+        $resultGet = $this->usersRepository->GetUserByUsername($data->username);
+
+        if ( ! empty($resultGet) )
+        {
+            $this->DTO->setStatusCode(406);
+            $this->DTO->setCode(5007);
+            $this->DTO->setMessage('There is already an user with the username provided. Try a different username instead.');
+
+            return $this->DTO;
+        }
+
         $result = $this->usersRepository->createUser($data);
 
         if ( $result <= 0 )
